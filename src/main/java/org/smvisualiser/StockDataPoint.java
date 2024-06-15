8babc9bc6b717406e19ceb69babe12ce764a922e
oscardilkes <OSCARDILKES@OUTLOOK.COM>
2024-06-15 14:10:34 +0100

    created stockdatapoint object, begun stockjsonparser

---
+++ src/main/java/org/smvisualiser/StockDataPoint.java
@@ -0,0 +1,45 @@
+package org.smvisualiser;
+
+public class StockDataPoint {
+
+  private final long timestamp;
+  private final double openPrice;
+  private final double closePrice;
+  private final double highPrice;
+  private final double lowPrice;
+  private final long volume;
+
+  public StockDataPoint(long timestamp, double openPrice, double closePrice, double highPrice, double lowPrice, long volume) {
+    this.timestamp = timestamp;
+    this.openPrice = openPrice;
+    this.closePrice = closePrice;
+    this.highPrice = highPrice;
+    this.lowPrice = lowPrice;
+    this.volume = volume;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public double getOpenPrice() {
+    return openPrice;
+  }
+
+  public double getClosePrice() {
+    return closePrice;
+  }
+
+  public double getHighPrice() {
+    return highPrice;
+  }
+
+  public double getLowPrice() {
+    return lowPrice;
+  }
+
+  public long getVolume() {
+    return volume;
+  }
+
+}
---
+++ src/main/java/org/smvisualiser/StockJsonParser.java
@@ -0,0 +1,12 @@
+package org.smvisualiser;
+
+import com.google.gson.JsonObject;
+
+import java.util.Set;
+
+public class StockJsonParser {
+  public Set<StockDataPoint> parseHistoricalDataPoints(JsonObject historicalData) {
+
+  }
+
+}
